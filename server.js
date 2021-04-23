55160c1a65ce3c33c992914e8a478eaef0ed0105
shinhonggyu <zowoz0@naver.com>
2021-04-23 19:46:11 +0900

    adding port

--- server.js
+++ server.js
@@ -40,8 +40,8 @@ app.post('/imageurl', (req, res) => {
   image.handleApicall(req, res);
 });
 
-app.listen(3000, () => {
-  console.log('app is running on port 3000'.yellow.bold);
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on port ${process.env.PORT}`.yellow.bold);
 });
 
 /*
